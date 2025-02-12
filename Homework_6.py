4d5bf9d9e51b17ee086c9fa5c9201699f03b012b
Sindorbek <sindorbekerkinov@gmail.com>
2025-02-12 22:15:21 +0500

    added

---
+++ Homework_6.py
@@ -0,0 +1,83 @@
+# while True:
+#     rang = input("Svetafor qaysi rangda? ").lower()
+#     if rang in ["qizil", "sariq", "yashil"]:
+#         print("Rahmat, to’g’ri keladi")
+#         break
+#     else:
+#         print("Xato rang, qayta urinib ko’ring.")
+
+
+# ringimport random
+# c = random.randint(1,11)
+# n=1
+# while True:
+#     a = float(input(f"Bir son o'yladim uni toping: "))
+#     if a==c:
+#         print("Tabriklayman topdingiz")
+#         print(f"{n}-urinishda topdingiz")
+#         break
+#     elif a>c:
+#         print("Men o'ylagan son undan kichik, qaytadan urinib ko'ring")
+#         n = n + 1
+#         continue
+#     elif a<c:
+#         print("Men o'ylagan son undan katta, qaytadan urinib ko'ring")
+#         n = n+1
+#         continue
+
+
+# Dostlar = []
+# ishora = True
+# n =1
+# while  ishora:
+#     a = input("Do'stlaringizni ismini kiriting:>>>")
+#     Dostlar.append(a)
+#     n=n+1
+#
+#     if a== "stop":
+#         ishora = False
+# print(Dostlar)
+
+
+# ishora = True
+# n= 1
+# while ishora:
+#     a = input("Qancha pul airboshlamoqchisiz  (Chiqish uchun exit dep yozing)>>> ")
+#     if a.lower() == "exit":
+#         ishora= False
+#         print("Dastur yakunlandi")
+#     else:
+#          c = int(a)
+#          b = c/12600
+#          print(f"{b} $")
+#          n= n+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
