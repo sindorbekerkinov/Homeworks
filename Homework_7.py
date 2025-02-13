68330d856c9a596896dfbdd1b829cc6f94377a5f
Sindorbek <sindorbekerkinov@gmail.com>
2025-02-13 14:09:05 +0500

    Create Homework_7.py

---
+++ Homework_7.py
@@ -0,0 +1,56 @@
+# def user_data(first_name=" ", last_name="",age=""):
+#     first_name = input("Ismingizni kiriting:")
+#     last_name = input("Familiyangizni kiriting:")
+#     age = input("Yoshingizni kiriting:")
+#
+#     print(f"Ism: {first_name.title()}\n"
+#           f"Familiya: {last_name.title()}\n"
+#           f"Yosh: {age}")
+# user_data()
+
+
+# def find_max(a="",b="",c=""):
+#     a = int(input("Birinchi sonni kiriting:"))
+#     b = int(input("Ikkinchi sonni kiriting:"))
+#     c = int(input("Uchinchi sonni kiriting:"))
+#     if a >= b and a >=c:
+#         print(f"Eng katta son {a}")
+#     elif b >=a and b >=c:
+#         print(f"Eng katta son {b}")
+#     elif c >=a and c >=b:
+#         print(f"Eng katta son {c}")
+# find_max()
+
+# def find_letter_count(word="",letter=""):
+#     word = input("Kiriting kiriting:")
+#     letter = input("Kiriting kiriting:")
+#     count = word.count(letter)
+#     print(f"{letter} {word}ga {count} ta bor.")
+# find_letter_count()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
