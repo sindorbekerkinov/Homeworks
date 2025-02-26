e0cb800498951bbee83f50f657884544e87de3ff
Sindorbek <sindorbekerkinov@gmail.com>
2025-02-26 23:09:53 +0500

    add

--- Homework_12/Homework.py
+++ Homework_12/Homework.py
@@ -1,13 +1,19 @@
 with open('pi_million_digits.txt') as file:
     pi = file.read()
+
 def check_num():
-    t_yil = input("Tug'ilgan sanangizni kiriting kiriting: ")
+    t_yil = input("Tug'ilgan sanangiz: ")
     ishora = True
     while ishora:
         if not t_yil in pi:
             ishora = False
             print("Sizning tug'ilgan sanangiz bu raqamlar ichida yo'q! ")
+
         else:
-            print("Sizning tug'ilgan kuniningiz bor:")
+            print("Sizning tug'ilgan kuniningiz bor! ")
+
+            import pickle
+            with open("info.dat","wb") as fayl:
+                pickle.dump(t_yil,fayl)
             break
 check_num()
