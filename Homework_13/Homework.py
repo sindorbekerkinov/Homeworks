c55bb6fd4803b729aa3ffced5027216ff907c51c
Sindorbek <sindorbekerkinov@gmail.com>
2025-03-04 15:54:32 +0500

    add

---
+++ Homework_13/Homework.py
@@ -0,0 +1,16 @@
+# import json
+# data = {"Model" : "Malibu", "Rang" : "Qora", "Yil":2020, "Narh":40000}
+# data_json = json.dumps(data)
+# print(data_json)
+# with open("data_json",'w') as file:
+#     json.dump(data,file,indent=3)
+#
+#
+
+
+# import json
+# talaba_json = {"ism":"Hasan","familiya":"Husanov","tyil":2000}
+# dtalaba_json = json.dumps(talaba_json)
+# with open("Talaba_json",'w') as file:
+#     json.dump(talaba_json,file,indent=3)
+
